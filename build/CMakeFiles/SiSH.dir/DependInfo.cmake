6419b95d6001cc159318b77121e5addebcb120bf
HOchacha <outcider112@dankook.ac.kr>
2024-09-30 02:19:13 +0900

    update

---
+++ build/CMakeFiles/SiSH.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/hochacha/repos/2024-os-hw1/src/argument_parser.c" "CMakeFiles/SiSH.dir/src/argument_parser.c.o" "gcc" "CMakeFiles/SiSH.dir/src/argument_parser.c.o.d"
+  "/home/hochacha/repos/2024-os-hw1/src/instruction_parser.c" "CMakeFiles/SiSH.dir/src/instruction_parser.c.o" "gcc" "CMakeFiles/SiSH.dir/src/instruction_parser.c.o.d"
+  "/home/hochacha/repos/2024-os-hw1/src/main.c" "CMakeFiles/SiSH.dir/src/main.c.o" "gcc" "CMakeFiles/SiSH.dir/src/main.c.o.d"
+  "/home/hochacha/repos/2024-os-hw1/src/shell.c" "CMakeFiles/SiSH.dir/src/shell.c.o" "gcc" "CMakeFiles/SiSH.dir/src/shell.c.o.d"
+  "/home/hochacha/repos/2024-os-hw1/src/tools/builtin_commands.c" "CMakeFiles/SiSH.dir/src/tools/builtin_commands.c.o" "gcc" "CMakeFiles/SiSH.dir/src/tools/builtin_commands.c.o.d"
+  "/home/hochacha/repos/2024-os-hw1/src/tools/path_finder.c" "CMakeFiles/SiSH.dir/src/tools/path_finder.c.o" "gcc" "CMakeFiles/SiSH.dir/src/tools/path_finder.c.o.d"
+  "/home/hochacha/repos/2024-os-hw1/src/tools/stack.c" "CMakeFiles/SiSH.dir/src/tools/stack.c.o" "gcc" "CMakeFiles/SiSH.dir/src/tools/stack.c.o.d"
+  "/home/hochacha/repos/2024-os-hw1/src/tools/string_tools.c" "CMakeFiles/SiSH.dir/src/tools/string_tools.c.o" "gcc" "CMakeFiles/SiSH.dir/src/tools/string_tools.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/SiSH.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SiSH.
+# This may be replaced when dependencies are built.
